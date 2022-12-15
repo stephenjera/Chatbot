17abea70c8e72e38c510ddc39887119b4e78a372
stephenjera <stephen.411@hotmail.co.uk>
2022-12-15 12:59:18 +0000

    Working chatbot demo

--- chatbot-python/chatbot_demo.py
+++ chatbot-python/chatbot_demo.py
@@ -1,10 +1,8 @@
-import json
 import string
 import random
 import nltk
-
-nltk.download("omw-1.4")
-import numpy as num
+import pandas as pd
+import numpy as np
 from nltk.stem import WordNetLemmatizer  # It has the ability to lemmatize.
 import tensorflow as tensorF  # A multidimensional array of elements is represented by this symbol.
 from tensorflow.keras import (
@@ -12,17 +10,15 @@ from tensorflow.keras import (
 )  # Sequential groups a linear stack of layers into a tf.keras.Model
 from tensorflow.keras.layers import Dense, Dropout
 
+nltk.download("omw-1.4")
 nltk.download("punkt")  # required package for tokenization
 nltk.download("wordnet")  # word database
 
+# Get schedule
+df = pd.read_excel("Schedule_Cleaned.xlsm")
 
 data = {
     "intents": [
-        {
-            "tag": "age",
-            "patterns": ["how old are you?"],
-            "responses": ["I am 2 years old and my birthday was yesterday"],
-        },
         {
             "tag": "greeting",
             "patterns": ["Hi", "Hello", "Hey"],
@@ -34,22 +30,46 @@ data = {
             "responses": ["Bye", "take care"],
         },
         {
-            "tag": "name",
-            "patterns": ["what's your name?", "who are you?"],
-            "responses": [
-                "I have no name yet," "You can give me one, and I will appreciate it"
+            "tag": "coach",
+            "patterns": ["Who is our data coach?", "Who is our Associate coach"],
+            "responses": ["Data Coach, Associate coach"],
+        },
+        {
+            "tag": "task",
+            "patterns": ["What is my task?", "show me a task"],
+            "responses": ["Data Coach, Associate coach"],
+        },
+        {
+            "tag": "tomorrow",
+            "patterns": [
+                "What is my task tomorrow?",
+                "show me a task next day",
+                "What are we covering in class tomorrow?",
+                "tomorrow",
             ],
+            "responses": ["Data Coach, Associate coach"],
         },
         {
-            "tag": "coach",
-            "patterns": ["Who is our data coach?", "Who is our Associate coach"],
-            "responses": ["Data Coach, Associate coach"]
+            "tag": "schedule",
+            "patterns": [
+                "What is my task tomorrow?",
+                "show me a task next day",
+                "What are we covering in class tomorrow?",
+            ],
+            "responses": ["Data Coach, Associate coach"],
         },
         {
-            "tag": "coach",
-            "patterns": ["Who is our data coach?", "Who is our Associate coach"],
-            "responses": ["Data Coach, Associate coach"]
-        }
+            "tag": "monday",
+            "patterns": [
+                "what am i doing on monday?",
+                "What is on monday",
+                "Tasks I need to complete on monday",
+                "Tasks for monday",
+                "Schedule for monday",
+                "Upcoming monday tasks",
+            ],
+            "responses": ["Data Coach, Associate coach"],
+        },
     ]
 }
 
@@ -92,12 +112,12 @@ for idx, doc in enumerate(documentX):
     trainingData.append([bagOfwords, outputRow])
 
 random.shuffle(trainingData)
-trainingData = num.array(
+trainingData = np.array(
     trainingData, dtype=object
 )  # coverting our data into an array afterv shuffling
 
-x = num.array(list(trainingData[:, 0]))  # first trainig phase
-y = num.array(list(trainingData[:, 1]))  # second training phase
+x = np.array(list(trainingData[:, 0]))  # first trainig phase
+y = np.array(list(trainingData[:, 1]))  # second training phase
 
 iShape = (len(x[0]),)
 oShape = len(y[0])
@@ -118,9 +138,9 @@ md = tensorF.keras.optimizers.legacy.Adam(learning_rate=0.01, decay=1e-6)
 # Below line improves the numerical stability and pushes the computation of the probability distribution into the categorical crossentropy loss function.
 ourNewModel.compile(loss="categorical_crossentropy", optimizer=md, metrics=["accuracy"])
 # Output the model in summary
-print(ourNewModel.summary())
+# print(ourNewModel.summary())
 # Whilst training your Nural Network, you have the option of making the output verbose or simple.
-ourNewModel.fit(x, y, epochs=200, verbose=1)
+ourNewModel.fit(x, y, epochs=200, verbose=0)
 # By epochs, we mean the number of times you repeat a training set.
 
 
@@ -137,12 +157,12 @@ def wordBag(text, vocab):
         for idx, word in enumerate(vocab):
             if word == w:
                 bagOwords[idx] = 1
-    return num.array(bagOwords)
+    return np.array(bagOwords)
 
 
 def Pclass(text, vocab, labels):
     bagOwords = wordBag(text, vocab)
-    ourResult = ourNewModel.predict(num.array([bagOwords]))[0]
+    ourResult = ourNewModel.predict(np.array([bagOwords]), verbose=0)[0]
     newThresh = 0.2
     yp = [[idx, res] for idx, res in enumerate(ourResult) if res > newThresh]
 
@@ -153,18 +173,23 @@ def Pclass(text, vocab, labels):
     return newList
 
 
-def getRes(firstlist, fJson):
-    tag = firstlist[0]
-    listOfIntents = fJson["intents"]
-    for i in listOfIntents:
-        if i["tag"] == tag:
-            ourResult = random.choice(i["responses"])
-            break
-    return ourResult
+# TODO use functiona as part of user interface
+def get_res(intents, df):
+    match intents[0]:
+        case "coach":
+            return df[["Coach/Associate", "Name"]]
+        case "greeting":
+            return random.choice(["Hi there", "Hello", "Hi :)"])
+        case "goodbye":
+            return random.choice(["Bye", "take care"])
+        case _:
+            return "I do not understand please ask a different question"
 
 
-while True:
-    newMessage = input("")
-    intents = Pclass(newMessage, newWords, ourClasses)
-    ourResult = getRes(intents, data)
-    print(ourResult)
+if __name__ == "__main__":
+    print("Hi, how can I help you today?")
+    while True:
+        newMessage = input("")
+        intents = Pclass(newMessage, newWords, ourClasses)
+        ourResult = get_res(intents, df=df)
+        print(ourResult)
